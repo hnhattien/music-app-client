37be9fc0f57b15be8ace3423feb69ee2c52a001c
hnhattien <tienit0105@gmail.com>
2023-06-01 12:36:46 +0700

    fix: remove packages

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import '@fortawesome/fontawesome-free/css/all.min.css'
-import 'bootstrap/dist/css/bootstrap.css';
-import {Button} from 'framework7'
-import Framework7 from 'framework7';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap/dist/css/bootstrap.css";
 
-import Framework7React from 'framework7';
-
-Framework7.use(Framework7React);
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
